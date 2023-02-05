6a3a6f02e0127214ca2a6efcf85fcd394d549d3b
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-02-05 18:43:19 +0500

    addbutton

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line no-undef
+module.exports = {
+	images: {
+		domains: ['courses-top.ru']
+	},
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			issuer: /\.[jt]sx?$/,
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{
+						name: 'preset-default',
+						params: {
+							override: {
+								removeViewBox: false
+							}
+						}
+					}],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
 
-module.exports = nextConfig
+		return config;
+	},
+};
