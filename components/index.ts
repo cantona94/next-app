7adeb00a36da4a23a9b52eb5d9ad0761d6f882d1
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-02-17 20:47:57 +0500

    useContext

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './P/P';
-export * from './Tag/TAg';
+export * from './Tag/Tag';
+export * from './Rating/Rating';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import { Button, Htag, P, Tag } from '../../components';
+import { GetStaticProps } from 'next';
+import { withLayout } from 'layout/Layout';
+import { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../../components';
+import axios from 'axios';
+import { MenuItem } from 'interfaces/menu.interface';
 
-export default function Home(): JSX.Element {
+function Home({menu}:HomeProps): JSX.Element {
+    const [rating, setRating] = useState<number>(3);
+
     return (
         <>
             <Htag tag="h1">текст</Htag>
@@ -13,11 +20,32 @@ export default function Home(): JSX.Element {
             <P size="l">Большой</P>
             <P>Средний</P>
             <P>Маленький</P>
-            <Tag color='red'>Текст</Tag>
-            <Tag color='primary'>
+            <Tag color="red">Текст</Tag>
+            <Tag color="primary">Текст</Tag>
+            <Tag size="m" color="green">
                 Текст
             </Tag>
-            <Tag size='m' color='green'>Текст</Tag>
+            <Rating rating={rating} isEditable setRating={setRating} />       
         </>
     );
+}
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown>{
+	menu: MenuItem[];
+	firstCategory: number;
 }
