dba895460c0f46d1684d59dd2d58816466bfaea5
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-02-27 20:54:09 +0500

    Сортировка

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,14 +1,26 @@
-import { Advantages, Htag, P, Tag } from 'components';
+import { Advantages, Htag, Sort, Tag } from 'components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { HhData } from 'components';
 import { TopLevelCategory } from 'interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
+import { useReducer } from 'react';
+import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({
     page,
     products,
     firstCategory,
 }: TopPageComponentProps): JSX.Element => {
+    const [{ products: sortedProducts, sort }, dispathSort] = useReducer(
+        sortReducer,
+        { products, sort: SortEnum.Rating }
+    );
+
+    const setSort = (sort: SortEnum) => {
+        dispathSort({ type: sort });
+    };
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>
@@ -18,11 +30,11 @@ export const TopPageComponent = ({
                         {products.length}
                     </Tag>
                 )}
-                <span>Сортировка</span>
+                <Sort sort={sort} setSort={setSort} />
             </div>
             <div>
-                {products &&
-                    products.map((p) => <div key={p._id}>{p.title}</div>)}
+                {sortedProducts &&
+                    sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}
             </div>
             <div className={styles.hhTitle}>
                 <Htag tag="h2">Вакансии - {page.category}</Htag>
@@ -33,14 +45,24 @@ export const TopPageComponent = ({
             {firstCategory == TopLevelCategory.Courses && page.hh && (
                 <HhData {...page.hh} />
             )}
-            {page.advantages && page.advantages.length > 0 && <>
-				<Htag tag='h2'>Преимущства</Htag>
-				<Advantages advantages={page.advantages} />
-			</>
-			}
-			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
-			<Htag tag='h2'>Получаемые навыки</Htag>
-			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
+            {page.advantages && page.advantages.length > 0 && (
+                <>
+                    <Htag tag="h2">Преимущства</Htag>
+                    <Advantages advantages={page.advantages} />
+                </>
+            )}
+            {page.seoText && (
+                <div
+                    className={styles.seo}
+                    dangerouslySetInnerHTML={{ __html: page.seoText }}
+                />
+            )}
+            <Htag tag="h2">Получаемые навыки</Htag>
+            {page.tags.map((t) => (
+                <Tag key={t} color="primary">
+                    {t}
+                </Tag>
+            ))}
         </div>
     );
 };
