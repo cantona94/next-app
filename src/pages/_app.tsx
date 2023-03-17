d0e801758abf36cb8e7e86c267ddd3dd50d74e8e
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-03-17 21:39:07 +0500

    Добавление meta на страницу

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import Head from "next/head";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Head from 'next/head';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({
+    Component,
+    pageProps,
+    router,
+}: AppProps): JSX.Element {
     return (
         <>
             <Head>
@@ -16,6 +20,11 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
                     content="width=device-width, initial-scale=1"
                 />
                 <link rel="icon" href="/favicon.ico" />
+                <meta
+                    property="og:url"
+                    content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+                />
+                <meta property="og:locale" content="ru_RU" />
             </Head>
             <Component {...pageProps} />
         </>
