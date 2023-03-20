3f4e4381ad5f8040fddc51547cdc7f5c57f0b13e
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-03-20 23:06:16 +0500

    Yandex метрика

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 export default function App({
     Component,
     pageProps,
     router,
 }: AppProps): JSX.Element {
+    router.events.on('routeChangeComplete', (url: string) => {
+        if (typeof window !== 'undefined') {
+            ym('hit', url);
+        }
+    });
+
     return (
         <>
             <Head>
@@ -20,12 +28,18 @@ export default function App({
                     content="width=device-width, initial-scale=1"
                 />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://mc.yandex.ru" />
                 <meta
                     property="og:url"
                     content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
                 />
                 <meta property="og:locale" content="ru_RU" />
             </Head>
+            <YMInitializer
+                accounts={[]}
+                options={{ webvisor: true, defer: true }}
+                version="2"
+            />
             <Component {...pageProps} />
         </>
     );
