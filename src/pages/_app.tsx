40580bb298169b6e5769c42b1c69d12c65321d08
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-03-20 23:39:02 +0500

    Next export

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from 'react-yandex-metrika';
+
+Router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+        ym('hit', url);
+    }
+});
 
 export default function App({
     Component,
     pageProps,
     router,
 }: AppProps): JSX.Element {
-    router.events.on('routeChangeComplete', (url: string) => {
-        if (typeof window !== 'undefined') {
-            ym('hit', url);
-        }
-    });
-
     return (
         <>
             <Head>
